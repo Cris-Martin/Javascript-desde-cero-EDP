a0547250760bb6811245f0cd216f2dba2c0d2c20
Cris-Martin <crismartin236@gmail.com>
2024-04-14 21:43:32 -0300

    respuestas

--- 02.Condicionales/02-condicionales.js
+++ 02.Condicionales/02-condicionales.js
@@ -2,6 +2,13 @@ function verificarNumero(numero){
     // Escribe un programa que tome un número como entrada y muestre "Es positivo" si el número es mayor que cero,
     // "Es negativo" si es menor que cero y "Es cero" si es igual a cero.
     // Tu código 👇:
+    if (numero > 0) {
+        console.log("Es positivo");
+    } else if (numero < 0) {
+        console.log("Es negativo");
+    } else {
+        console.log("Es cero");
+    }
 
 }
 
@@ -10,7 +17,15 @@ function esBisiesto(anio){
     // Escribe un programa que determine si un año es bisiesto o no. (true o false)
     // Un año es bisiesto si es divisible entre 4, excepto aquellos que son divisibles entre 100 pero no entre 400.
     // Tu código 👇:
+    if ((año % 4 === 0 && año % 100 !== 0) || año % 400 === 0) {
+        return true; // Es bisiesto
+    } else {
+        return false; // No es bisiesto
+    
 
+    
+
+    }
 }
 
 function verificarCalificacion(calificacion){
@@ -18,22 +33,43 @@ function verificarCalificacion(calificacion){
     // El estudiante aprueba si su calificación es mayor o igual a 60.
     // Devolver "Aprobó" o "Reprobó"
     // Tu código 👇:
-
+    if (calificacion >= 60) {
+        return "Aprobó";
+    } else {
+        return "Reprobó";
+    }
 }
+
 
 function esMayor(num1, num2, num3){
     // Escribe un programa que determine el mayor de tres números dados.
     // Devolver el numero mayor
     // Tu código 👇:
-
+    if (num1 >= num2 && num1 >= num3) {
+        return num1;
+    } else if (num2 >= num1 && num2 >= num3) {
+        return num2;
+    } else {
+        return num3;
+    }
 }
 
 function esPrimo(num){
     // Escribe un programa que determine si un número dado es primo.
     // Devolver true o false
     // Tu código 👇:
-
+    if (numero <= 1) {
+        return false;
+    }
+    for (let i = 2; i <= Math.sqrt(numero); i++) {
+        if (numero % i === 0) {
+            return false;
+        }
+    }
+    return true;
 }
+
+
 
 module.exports = {
     verificarCalificacion,
